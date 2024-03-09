6792076220079a399ad2b14659f0124f3c44e756
viktorhugo <viktor1103@gmail.com>
2024-03-08 23:15:12 -0500

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GlobalModule } from './global/global-module.module';
 import { EventsModule } from './web-socket/events.module';
 
@@ -14,7 +12,5 @@ import { EventsModule } from './web-socket/events.module';
       rootPath: join(__dirname, '..', 'client'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
